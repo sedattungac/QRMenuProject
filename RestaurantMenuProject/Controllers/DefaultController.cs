20ac3b9d62f34df0ef5b9c0ee9f265c1d2fb542d
Sedat <sedattungac12@gmail.com>
2022-10-27 18:38:52 +0300

    Add project files.

---
+++ RestaurantMenuProject/Controllers/DefaultController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantMenuProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        FoodCategoryManager foodCategoryManager = new FoodCategoryManager(new EfFoodCategoryDal());
+        FoodManager foodManager = new FoodManager(new EfFoodDal());
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            var values = foodCategoryManager.TGetList().Where(x => x.Status == true).ToList();
+            return View(values);
+        }
+        public IActionResult MenuDetail(int id)
+        {
+            var value = context.Foods.Where(x => x.FoodCategoryId == id).ToList();
+            ViewBag.category = context.FoodCategories.
+                Where(x => x.FoodCategoryId == id).Select(x => x.Title).FirstOrDefault();
+            return View(value);
+        }
+    }
+}
